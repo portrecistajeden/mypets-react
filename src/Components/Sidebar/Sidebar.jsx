439843c31320a09614f38de7ccc9cec91721281f
wi104036 <piawramiuk@gmail.com>
2023-05-17 00:02:51 +0200

    added active button color

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -9,14 +9,15 @@ import {FaClinicMedical} from 'react-icons/fa';
 export default function Sidebar() {
 
     const [sidebarHidden, setSidebarHidden] = useState(true);
+    const [activePage, setActivePage] = useState('Pets');
 
     return(
         <div className={`overflow-hidden relative flex flex-col justify-between h-screen bg-emerald-200 ${sidebarHidden ? 'w-20' : 'w-52'} transition-width duration-200 ease-out`}>
             <div className="flex flex-col">
                 <Logo sidebarHidden={sidebarHidden}/>
-                <SidebarLink target={'/user/pets'} text='Pets' icon={<RiUser3Line/>} sidebarHidden = {sidebarHidden}/> 
-                <SidebarLink target={'/user/appointments'} text='Appointments' icon={<FaClinicMedical/>} sidebarHidden = {sidebarHidden}/> 
-                <SidebarLink target={'/user/history'} text='History' icon={<BsCalendarDate/>} sidebarHidden = {sidebarHidden}/> 
+                <SidebarLink data={{target: '/user/pets', text: 'Pets', icon:<RiUser3Line/>, sidebarHidden: sidebarHidden, setActivePage: setActivePage, activePage: activePage}}/> 
+                <SidebarLink data={{target: '/user/appointments', text: 'Appointments', icon:<FaClinicMedical/>, sidebarHidden: sidebarHidden, setActivePage: setActivePage, activePage: activePage}}/> 
+                <SidebarLink data={{target: '/user/history', text: 'History', icon:<BsCalendarDate/>, sidebarHidden: sidebarHidden, setActivePage: setActivePage, activePage: activePage}}/> 
             </div>
             <VisibilityButton sidebarHidden={sidebarHidden} setSidebarHidden={setSidebarHidden}/>
         </div>
--- src/Components/Sidebar/SidebarLink.jsx
+++ src/Components/Sidebar/SidebarLink.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function SidebarLink({target, text, icon, sidebarHidden}) {
-  return (
-    <div className='flex justify-center items-center w-full h-16 my-2'>
-        <Link to={target} className={`flex flex-row px-4 items-center text-xl w-16 h-16 bg-emerald-300 rounded-[32px] hover:rounded-[6px]  duration-200 mx-0 ${sidebarHidden ? 'justify-center' : 'w-48'}`}>
-            <div className={` overflow-hidden flex justify-center items-center h-8 ${sidebarHidden ? '' : 'mr-2'}`}>
-                {icon}
-            </div>
+export default function SidebarLink({data}) {
+    
+    const isActive = (text) => {
+        return (data.activePage===text);
+    }
 
-            {sidebarHidden ?
-                <></>
-                :
-                text
-            }
-        </Link>
-    </div>
+    return (
+        <div className='flex justify-center items-center w-full h-16 my-2'>
+            <Link onClick={() => data.setActivePage(data.text)} to={data.target} className={`flex flex-row px-4 items-center text-xl w-16 h-16 ${isActive(data.text) ? 'bg-emerald-400' : 'bg-emerald-300'} rounded-[32px] hover:rounded-[6px] duration-200 mx-0 ${data.sidebarHidden ? 'justify-center' : 'w-48'}`}>
+                <div className={`flex justify-center items-center h-8 ${data.sidebarHidden ? '' : 'mr-2'}`}>
+                    {data.icon}
+                </div>
+                
+                <div className='overflow-hidden '>
+                {data.sidebarHidden ?
+                    <></>
+                    :
+                    data.text
+                }
+                </div>
+            </Link>
+        </div>
   )
 }
