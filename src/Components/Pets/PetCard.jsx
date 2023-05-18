b4684f017c960664df6ff61868dfb5f9ef3a75ce
wi104036 <piawramiuk@gmail.com>
2023-05-18 23:10:09 +0200

    well. now done

--- src/Components/Pets/PetCard.jsx
+++ src/Components/Pets/PetCard.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function PetCard({data}) {
   return (
-    <div className='relative flex flex-col items-center w-1/4 min-h-[280px] max-h-[350px] min-w-[280px] max-w-[350px] aspect-[1/1] mx-10 hover:scale-105 transition'>
+    <Link to='/#' className='relative flex flex-col items-center w-1/4 min-h-[280px] max-h-[350px] min-w-[280px] max-w-[350px] aspect-[1/1] mx-10 hover:scale-105 transition'>
         <div id=' photoWrapper' className='w-fit h-1/3 absolute top-0 z-30 drop-shadow-md shadow-stone-300'>
             <img className='rounded-full border-4 border-accent h-full w-full object-cover' src={data.imgSource}/>
         </div>
@@ -11,7 +12,7 @@ export default function PetCard({data}) {
                 <h2 className='mx-auto text-2xl p-2 underline-offset-8 underline'>
                     Vinci Goliszek
                 </h2>
-                <div className='flex flex-col justify-center grow bg-primary p-2 m-2 mt-1 2xl:mt-6 rounded-lg text-lg shadow-inner'>
+                <div className='flex flex-col justify-center grow bg-primary p-2 m-2 mt-1 2xl:mt-4 rounded-lg text-lg shadow-inner'>
                     <h2 className=''>
                         Species: Dog 
                     </h2>
@@ -26,6 +27,6 @@ export default function PetCard({data}) {
                     </h2>
                 </div>
         </div>
-    </div>
+    </Link>
   )
 }
--- src/Pages/Pets.jsx
+++ src/Pages/Pets.jsx
@@ -13,7 +13,7 @@ export default function Pets({data}) {
     console.log('well ' + getUser(data.user.email))
 
     return(
-        <div className="w-full h-fit flex flex-row justify-center items-center p-8">
+        <div className="w-full h-1/3 flex flex-row justify-center items-center p-8">
             <PetCard data={{imgSource: vincijpg}}/>
             <PetCard data={{imgSource: vincijpg}}/>
             <PetCard data={{imgSource: vincijpg}}/>
